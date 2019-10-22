a02170076c5d7ea59523748df19d30711d84edf8
stefan <252556310@qq.com>
2019-10-22 15:59:00 +0800

    完善响应拦截器的逻辑

--- apis/http.js
+++ apis/http.js
@@ -1,15 +1,12 @@
 import store from "@/store"
 function HTTP(obj, config, callback) {
-
+	// 默认配置
 	let defaultConfig = {
 		isRes: false,     // 是否需要直接返回请求的结果
 		loading: false    // 是否需要显示"加载中"的提示
 	}
-
-	config = { ...defaultConfig,
-		...config
-	}
-
+	// 合并默认配置和用户自定义配置
+	config = { ...defaultConfig, ...config};
 
 	// 如果需要显示loading,mask防止点击穿透
 	config.loading && uni.showLoading({
@@ -22,7 +19,6 @@ function HTTP(obj, config, callback) {
 	console.log('请求拦截处111')
 
 	return new Promise((resolve, reject) => {
-
 		let options = {
 			url: "",
 			method: "GET",
@@ -33,27 +29,39 @@ function HTTP(obj, config, callback) {
 				"X-requested-With": "XMLHttpRequest"
 			},
 			success: (res) => {
-				console.log("HTTP请求结果：",res)
+				console.log("HTTP请求结果： ", res)
 				uni.hideLoading();
 				// 状态码为200
 				if (res.statusCode == 200) {
 					// 这里可以当作"响应拦截器"使用
 					// 可以对请求的响应数据做初步的统一的处理
 					console.log('响应拦截处')
+					
+					// 服务器端返回的数据格式
+					// {
+					// 	  code: 'expired',  // 可选的(一般涉及到登录验证接口的时候才会返回这个字段)
+					// 	  success: true,
+					// 	  data: [] | {},
+					// 	  error: "error info"
+					// }
 					let data = res.data;
 					
-					// 自动校验用户是否登录过期
-					if (data.code == "expired" || data.code == "notoken" || data.code == "wrongtoken" || data.code == "noopenid") {   
-						store.dispatch("reLogin");
-						return;
+					if (typeof data.code != 'undefined') {
+						// 自动校验用户是否登录过期
+						if (data.code == "expired" || data.code == "notoken" || data.code == "wrongtoken" || data.code == "noopenid") {   
+							store.dispatch("reLogin");
+							return;
+						}
+						// 还可以对code进行其他判断处理
+						if (data.code == "xxx") {
+							// do something
+						}
 					}
 
-					// 返回 { code:10000,msg:"消息",data:[] }
-					// 返回 { code:10000,error:"消息",data:[] }
+					// 直接返回完整的请求结果
 					if (config.isRes) {
-						// 直接返回请求结果数据
 						resolve(data)
-					} else { // 返回 data:[]
+					} else {  // 只返回请求结果中的数据部分
 					
 						if (data.success) {
 							resolve(data.data)
@@ -65,19 +73,6 @@ function HTTP(obj, config, callback) {
 							})
 							reject(data.error);
 						}
-					
-						// data.code = '00'
-						
-						// if (data.code == "00") {
-						// 	resolve(data.data || true)
-						// } else {
-						// 	wx.showToast({
-						// 		title: data.error,
-						// 		icon: "none",
-						// 		duration: 2000
-						// 	})
-						// 	reject(data.error);
-						// }
 					}
 				} else {
 					reject("HTTP:状态码异常！");
